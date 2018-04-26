add80cece09bed3ba6218cb27fb628404b7d1a48
181x029x <38451504+181x029x@users.noreply.github.com>
2018-04-26 16:42:08 +0900

    Add files via upload

--- w06/main02w06.js
+++ w06/main02w06.js
@@ -23,12 +23,12 @@ function main()
 
     var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
     var material = new THREE.ShaderMaterial({
-        vertexColors: THREE.VertexColors,
-        vertexShader: document.getElementById('gourand.vert').text,
-        fragmentShader: document.getElementById('gourand.frag').text,
-          uniforms: {
-        light_position: { type: 'v3', value: light.position }
-        }
+       vertexColors: THREE.VertexColors,
+       vertexShader: document.getElementById('gourand.vert').text,
+       fragmentShader: document.getElementById('gourand.frag').text,
+       uniforms: {
+      	 light_position: { type: 'v3', value: light.position }
+      	}
     });
 
     var torus_knot = new THREE.Mesh( geometry, material );
